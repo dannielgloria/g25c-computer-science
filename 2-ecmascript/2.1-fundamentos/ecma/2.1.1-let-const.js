2ad93da1e92f4aa48086b0701bece41db08630e8
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-06-05 19:38:26 -0600

    feat: es6.1 let y const

---
+++ 2-ecmascript/2.1-fundamentos/ecma/2.1.1-let-const.js
@@ -0,0 +1,47 @@
+// var: Permite definir una variable en un scope global.
+/**
+ * No es la mejor practica utilizar ya que podemos sobreeescribir valores y generar errores
+ */
+var titlePage = 'Dev.f .io';
+
+// let: Permite definir una variable en un scope especifico. // es6
+function sum(num1, num2) {
+    // ERROR porque result solo estaria alcanzable dentro if (scope/ambito de vida)
+    // if (true) {
+    //     let result = num1 + num2;
+    // }
+    let result = num1 + num2;
+    console.log(result);
+    return result;
+}
+// console.log(result);
+console.log(sum(20, 50));
+
+var n1 = 40;
+var n2 = 20;
+function substract(n1, n2) {
+    var resultSubs = n1 - n2;
+    return resultSubs;
+}
+// console.log(resultSubs);
+
+console.log(substract(20, 10)); // 10
+n1 = 90;
+console.log(substract(20, 10)); // 10
+
+// https://udn.realityripple.com/docs/Web/JavaScript/Reference/Errors/Not_defined
+
+let name;
+name = 'daniel';
+console.log('name:', name);
+name = 'griselda';
+console.log('name:', name);
+
+// const app;
+// app = 'montoya';
+// Uncaught SyntaxError: Missing initializer in const declaration
+
+const app = 'montoya';
+console.log('app:', app);
+// app = 'mendez'; // Uncaught TypeError: Assignment to constant variable.
+console.log('app:', app);
