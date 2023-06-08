f99da8b3a843403ad3d66a072e2f8cb7c9ba9072
dannielgloria <danniel.gloria@gmail.com>
2023-06-07 21:28:31 -0600

    tarea: Programacion orientada a objetos y prototype

---
+++ 7-tareas/7.4-poo-prototype.js
@@ -0,0 +1,24 @@
+/*
+Herencia de clases:
+a) Crea una clase llamada "Animal" con las propiedades "nombre" y "edad". Luego, crea 
+una clase llamada "Perro" que herede de la clase "Animal" y tenga una propiedad 
+adicional llamada "raza". Añade métodos para obtener y establecer la raza del perro.
+
+Herencia de prototipos:
+a) Crea un objeto prototipo llamado "Vehiculo" con propiedades como "marca" y "modelo".
+Luego, crea un objeto llamado "Coche" que herede del prototipo "Vehiculo" y tenga una 
+propiedad adicional llamada "color". Añade métodos para obtener y establecer el color 
+del coche.
+
+Uso de prototipos:
+a) Crea una función constructora llamada "Persona" con propiedades como "nombre" y 
+"edad". Añade un método al prototipo de "Persona" llamado "saludar" que muestre un 
+mensaje de saludo con el nombre de la persona. Crea una instancia de "Persona" y 
+llama al método "saludar".
+
+Uso de "Object.create":
+a) Crea un objeto prototipo llamado "Forma" con un método llamado "calcularArea". 
+Luego, crea un objeto llamado "Circulo" usando "Object.create" y define su radio. 
+Añade un método al objeto "Circulo" que calcule el área del círculo utilizando el 
+método heredado de "Forma".
+*/
